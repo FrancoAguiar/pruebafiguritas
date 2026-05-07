b7f777ec7558142035dbf0eeb3e1d53162df78bc
Franco Aguiar <131722274+FrancoAguiar@users.noreply.github.com>
2026-05-07 18:14:42 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='figuritapp-v12-camera-ocr';
+const CACHE_NAME='figuritapp-v13-camera-fix-1';
 const ASSETS=['./','./index.html','./albumData.js','./service-worker.js','./logo.png','./icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).catch(()=>{})));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
